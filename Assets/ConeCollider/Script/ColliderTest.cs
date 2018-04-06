ed3d95b5d332fb5bb6c9c40a5f5123337f60870a
Aser <asermalik@live.com>
2018-04-06 15:57:20 -0400

    One

---
+++ Assets/ConeCollider/Script/ColliderTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderTest : MonoBehaviour {
+    public float rad = 1f;
+    public float hgt = 3f;
+    private float score = 0f;
+    public GameObject objectToAnimate;
+    private Animator theAnimator;
+
+    CapsuleCollider thisone;
+	// Use this for initialization
+	void Start () {
+        thisone = GetComponent<CapsuleCollider>();
+        theAnimator = objectToAnimate.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            thisone.enabled = true;
+            Debug.Log("Collider.enabled = " + thisone.enabled);
+            theAnimator.GetComponent<Animation>().Play("one");
+            Debug.Log(theAnimator.GetComponent<Animation>());
+        } else {
+            thisone.enabled = false;
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("CollisionEnter \"" + other.gameObject.name + " \"Object");
+        if (other.gameObject.tag == "Chip"){
+            other.gameObject.health--;
+            score = score + 0.5f;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("CollisionStay \"" + other.gameObject.name + " \"Object");
+               if (other.gameObject.tag == "Chip"){
+            other.gameObject.health--;
+            score++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("CollisionExit \"" + other.gameObject.name + " \"Object");
+               if (other.gameObject.tag == "Chip"){
+            other.gameObject.health--;
+            score++;
+        }
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{ 
+private float Speed;
+public float walkSpeed;
+public float runSpeed;
+private Vector3 _velocity;
+public Vector3 Drag;
+public float DashDistance;
+public float landShockDuration;
+public float cooldown;
+private float timestamp;
+private float timestamp2;
+private CharacterController _controller;
+
+void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+		Speed = runSpeed;
+    }
+
+void Update()
+    {
+
+    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    _controller.Move(move * Time.deltaTime * Speed);
+
+	if (move != Vector3.zero)
+            transform.forward = move;
+
+	if (Input.GetButtonDown("Walk"))
+	{
+		Speed = walkSpeed;
+	}
+	if (Input.GetButtonUp("Walk"))
+	{
+		Speed = runSpeed;
+	}
+
+ 	if (Input.GetKeyDown(KeyCode.Space))
+	{
+		if (timestamp <= Time.time) //once total + cooldown time reaches realtime, re-enable
+		{
+			timestamp = Time.time + cooldown;
+            _velocity += Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * Drag.x + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * Drag.z + 1)) / -Time.deltaTime)));
+		 	Speed = 1;
+			Invoke("resetSpeed", landShockDuration);
+		 }
+    }
+
+		_velocity.x /= 1 + Drag.x * Time.deltaTime;
+		_velocity.z /= 1 + Drag.z * Time.deltaTime;
+
+	 	_controller.Move(_velocity * Time.deltaTime);
+    }	 
+	void resetSpeed()
+	{
+		Speed = runSpeed;
+	}
+}
+
